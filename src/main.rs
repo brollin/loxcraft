10faa8a1d981f826391cab34d720437679e84e4b
Ben Rollin <ben.rollin@clever.com>
2024-07-10 18:56:05 -0700

    do pseudo-oop things

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,60 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
+    let mut lox = Lox { had_error: false };
     let args: Vec<String> = env::args().collect();
     if args.len() > 2 {
         eprintln!("Usage: loxcraft [script]");
-        std::process::exit(64);
+        process::exit(64);
     } else if args.len() == 2 {
-        run_file(&args[1]);
+        lox.run_file(&args[1]);
     } else {
-        run_prompt();
+        lox.run_prompt();
     }
 }
 
-fn run_file(path: &str) {
-    let source = fs::read_to_string(path).expect("Failed to read file");
-    run(&source);
+pub struct Lox {
+    had_error: bool,
 }
 
-fn run_prompt() {
-    loop {
-        let mut line = String::new();
-        match std::io::stdin().read_line(&mut line) {
-            Ok(_) => run(&line),
-            Err(_) => break,
+impl Lox {
+    pub fn run_file(&self, path: &str) {
+        let source = fs::read_to_string(path).expect("Failed to read file");
+        self.run(&source);
+
+        if self.had_error {
+            process::exit(65);
         }
     }
-}
 
-fn run(source: &str) {
-    println!("{}", source);
+    pub fn run_prompt(&mut self) {
+        loop {
+            let mut line = String::new();
+            match std::io::stdin().read_line(&mut line) {
+                Ok(_) => self.run(&line),
+                Err(_) => break,
+            }
+            self.had_error = false;
+        }
+    }
+
+    pub fn run(&self, source: &str) {
+        let scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens();
+
+        for token in tokens {
+            println!("{token}");
+        }
+    }
+
+    pub fn error(&mut self, line: usize, message: &str) {
+        self.report(line, "", message);
+    }
+
+    pub fn report(&mut self, line: usize, location: &str, message: &str) {
+        eprintln!("[line {}] Error {}: {}", line, location, message);
+        self.had_error = true;
+    }
 }
