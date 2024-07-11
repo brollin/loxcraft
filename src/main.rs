03c2631e83a0e43ed911d1d336c405d786cc9e86
Ben Rollin <ben.rollin@clever.com>
2024-07-10 18:18:06 -0700

    implement interpreter skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        eprintln!("Usage: loxcraft [script]");
+        std::process::exit(64);
+    } else if args.len() == 2 {
+        run_file(&args[1]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &str) {
+    let source = fs::read_to_string(path).expect("Failed to read file");
+    run(&source);
+}
+
+fn run_prompt() {
+    loop {
+        let mut line = String::new();
+        match std::io::stdin().read_line(&mut line) {
+            Ok(_) => run(&line),
+            Err(_) => break,
+        }
+    }
+}
+
+fn run(source: &str) {
+    println!("{}", source);
 }
